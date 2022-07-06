4187aa0364c5a75141937fc1b1dc317406b2ae66
Yevheniia Suska <susskaya86@gmail.com>
2022-07-07 02:16:01 +0300

    commented warnings

--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import s from './FriendsList.module.css';
 
 export const FriendsList = ({ data }) => {
   console.log('data', data);
-  return <div>FriendsList</div>;
+  return <div className={s.card}></div>;
 };
 
 FriendsList.propTypes = {};
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import s from './Profile.module.css';
 
 export const Profile = ({ data }) => {
